a23d2da82a0ee40078dbd1ccb74c9da3954214b6
Luca Heidland <luca.heidland@gmail.com>
2024-11-11 13:48:30 +0100

    Wie es ausschaut, wenn man vergisst ne .gitignore zu machen

--- Wortgitter/Controllers/ImageController.cs
+++ Wortgitter/Controllers/ImageController.cs
@@ -1,12 +1,15 @@
 ﻿using IronOcr;
+using Microsoft.AspNetCore.Mvc;
 using Wortgitter.Services;
 
 namespace Wortgitter.Controllers
 {
+    [Controller]
     public class ImageController : IImageController
     {
         private readonly IImageService _imageService;
-        
+
+        [HttpPost]
         public OcrResult ReadImage(string path)
         {
             OcrResult image = _imageService.ReadImage(path);
--- Wortgitter/Program.cs
+++ Wortgitter/Program.cs
@@ -8,6 +8,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -17,7 +18,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
